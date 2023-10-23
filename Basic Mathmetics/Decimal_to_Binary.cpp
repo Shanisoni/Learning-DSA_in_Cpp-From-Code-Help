ccba945529111ab242fb94dddaff844b2d1eb2fd
Shani Soni <shanisoni7084@gmail.com>
2023-10-23 06:56:11 -0700

    Method_2 Decimal to Binary

--- Basic Mathmetics/Decimal_to_Binary.cpp
+++ Basic Mathmetics/Decimal_to_Binary.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
+#include<cmath>
 using namespace  std;
 int main ()
 {
-    int n ;
-    cin >> n ;
-    while(n>0){
-        int a = n % 2 ;
-        cout << a ; 
-        n = n / 2 ;
+//     int n ;
+//     cin >> n ;
+//     while(n>0){
+//         int a = n % 2 ;
+//         cout << a ; 
+//         n = n / 2 ;
       
-    } 
+//     } 
+//  return 0 ;
+
+int n ;
+cin >> n ; 
+int binary = 0 ; 
+int i = 0 ; 
+ while( n > 0 ) {
+    int bit = n & 1 ;
+    binary = bit*pow(10, i++) + binary ;
+    cout << binary << endl ;
+    n = n >> 1 ; 
+
+ }
+
+
  return 0 ;
 }
