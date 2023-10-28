8a0fd8c2bdac49a7d54b4956e2c7b6f8a902945d
Shani Soni <shanisoni7084@gmail.com>
2023-10-28 01:23:19 -0700

    Conditions & Loops

---
+++ Conditional  & Loops/Conditions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
+ int main ()
+ {
+    int a = 6 ;
+    if(a >= 10)
+    {
+        cout<< "shani" ;
+    } 
+    else {
+        cout<< "soni" ;
+    }
+    return 0 ;
+    
+ }
